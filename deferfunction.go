50e2bb39117b3d7034c9654a3fc92a3db9325088
Neeraj <neerajgirdhar@gmail.com>
2025-11-07 08:12:26 +0530

    Updated

--- deferfunction.go
+++ deferfunction.go
@@ -1,35 +1,28 @@
 package main
-
 import "fmt"
-
 func removeEmployeeswithCode8000(map1 map[int]string, startswith int) map[int]string {
 	fmt.Println("This is Defered function")
-	map2 := map1
-	for key, value := range map2 {
+	for key, value := range map1 {
 		if startswith == key {
 			fmt.Printf("Deleting Value %s \n", value)
-			delete(map2, key)
+			delete(map1, key)
 
 		}
 	}
-	return map2
+	return map1
 }
-
 func removeEmployees(map1 map[int]string, startswith int) map[int]string {
-
 	fmt.Println("Deleting employee with ", startswith)
 	defer removeEmployeeswithCode8000(map1, 8000)
-	map2 := map1
-	for key, value := range map2 {
+	for key, value := range map1 {
 		if startswith == key {
 			fmt.Printf("Deleting Value %s \n", value)
-			delete(map2, key)
+			delete(map1, key)
 
 		}
 	}
-	return map2
+	return map1
 }
-
 func main() {
 	namesmap := make(map[int]string)
 	namesmap[1001] = "Alice"
@@ -42,6 +35,6 @@ func main() {
 	namesmap[8000] = "John"
 
 	namesmap = removeEmployees(namesmap, 1001)
-	fmt.Println("Defer also callled")
+	fmt.Println("Defer also called")
 	fmt.Println(namesmap)
 }
